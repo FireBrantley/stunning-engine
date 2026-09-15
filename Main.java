0b20c755b322c384d701e0d4790082582ee50525
Fire <123254541+FireBrantley@users.noreply.github.com>
2026-09-15 14:13:37 +0000

    Add basic engine class

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Engine steam = new Engine("Steam");
+
+        System.out.println();
+        System.out.println(steam);
+    }
+}
