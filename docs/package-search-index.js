c836ce6bdc3043709d8487ce682665ab392ffa46
Fire <123254541+FireBrantley@users.noreply.github.com>
2026-09-18 01:33:45 +0000

    Updated documentation version after last update

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"src.component"},{"l":"src.vehicles"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"src.components"},{"l":"src.vehicles"}];updateSearchResults();
